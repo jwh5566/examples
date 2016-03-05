d2326e1eac61d746ada2bf1c7c5542a505c5ebfb
jwh5566 <wenhua8@gmail.com>
2016-03-05 23:10:07 +0800

    站点地图抓取更新

--- day1/test1.py
+++ day1/test1.py
@@ -3,6 +3,7 @@
     从站点地图下载
 """
 import urllib2
+import re
 
 
 def download(url, user_agent='wswp', num_retries=2):
@@ -19,4 +20,17 @@ def download(url, user_agent='wswp', num_retries=2):
                 return download(url, user_agent, num_retries-1)
     return html
 
+
+def crawl_sitemap(url):
+    # download the sitemap file
+    sitemap = download(url)
+    # extract the sitemap links
+    links = re.findall('<loc>(.*?)</loc>', sitemap)
+    # download each link
+    for link in links:
+        html = download(link)
+        # scrape html here
+        # ...
+
 # download('http://httpstat.us/500')
+crawl_sitemap('http://example.webscraping.com/sitemap.xml')
